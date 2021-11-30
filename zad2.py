7dba7672c6ac1fab4e7cf4b0fd90b3e0ff1dcb95
wolnikowa <78367914+wolnikowa@users.noreply.github.com>
2021-11-30 22:30:33 +0100

    zad2 finished

--- zad2.py
+++ zad2.py
@@ -1,5 +1,4 @@
-# import unittest
-# from parameterized import *
+
 class FizzBuzz:
     def game(self, num):
         if (type(num) is int):
@@ -13,55 +12,6 @@ class FizzBuzz:
                 return num
         else:
             raise Exception("Wrong value type")
-# class testFizzBuzz(unittest.TestCase):
-#     def setUp(self):
-#         self.temp =FizzBuzz()
-#     @parameterized.expand([
-#         (5,"Buzz"),
-#         (3,"Fizz"),
-#         (15, "FizzBuzz"),
-#         (-15, "FizzBuzz"),
-#         (13, 13),
-#         (-16, -16)
-#     ])
-#     def test_parameterized(self,number, expectedOutput):
-#         self.assertEqual(self.temp.game(number), expectedOutput)
-#
-# @parameterized_class(('num', 'expectedOutput'), [
-#     (5, "Buzz"),
-#     (3, "Fizz"),
-#     (15, "FizzBuzz"),
-#     (-15, "FizzBuzz"),
-#     (13, 13),
-#     (-16, -16)
-# ])
-#
-# class testFizzBuzzClass(unittest.TestCase):
-#     def setUp(self):
-#         self.tmp = FizzBuzz()
-#
-#     def test_parameterized_class(self):
-#         self.assertEqual(self.tmp.game(self.num), self.expectedOutput)
-
-# @parameterized_class(('exception'), [
-#     ("trzy"),
-#     ("3"),
-#     ([1,2,3]),
-#     ({}),
-#     (False),
-#     (None)
-# ])
-#
-# class testFizBuzzClasException(unittest.TestCase):
-#     def setUp(self):
-#         self.tmp = FizzBuzz()
-#
-#     def test_parameterized_exceptions(self):
-#         self.assertRaises(Exception, self.tmp.game, self.exception)
-
-# if __name__ == '__main__':
-#     unittest.main()
-
 
 
 from parameterized import parameterized, parameterized_class
